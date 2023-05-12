88cd529d54b989bd7d823eaa79a2d6c08b76e51d
DavidTorres09 <angeldtm09@gmail.com>
2023-05-12 09:11:17 -0400

    first commit

---
+++ server/src/modules/vault/vault.controller.ts
@@ -0,0 +1,27 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { get } from "lodash";
+import { updateVault } from "./vault.service";
+import { logger } from "@typegoose/typegoose/lib/logSettings";
+
+export async function updateVaultHandler(
+  request: FastifyRequest<{
+    Body: {
+      encryptedVault: string;
+    };
+  }>,
+  reply: FastifyReply
+) {
+  const userId = get(request, "user._id");
+
+  try {
+    await updateVault({
+      data: request.body.encryptedVault,
+      userId,
+    });
+
+    return reply.code(200).send("Vault updated");
+  } catch (error) {
+    logger.error(error, "error updating vault")
+    return reply.code(500).send(error);
+  }
+}
